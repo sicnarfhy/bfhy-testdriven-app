902d1257260fb93a53f9c88772eecac0262a4f9e
Benazir <benazir.herrera@gmail.com>
2018-11-25 11:34:28 -0500

    lesson 5

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,6 @@
 # services/users/project/__init__.py
 
-
+import os  # new
 from flask import Flask, jsonify
 
 
@@ -8,8 +8,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+#app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
 
+import sys
+#print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 
